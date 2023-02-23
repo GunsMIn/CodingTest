58878bbe78d6c3be76bc8b0c58f50e14e9c5903e
KimGunWoo9595 <medneter@naver.com>
2023-02-23 17:44:13 +0900

    h-index

---
+++ src/main/java/codingTest/day3/Hindex.java
@@ -0,0 +1,15 @@
+package codingTest.day3;
+
+import java.util.Arrays;
+
+public class Hindex {
+
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        // 0 1 3 5 6
+        for (int i = 0; i < citations.length; i++) {
+            
+        }
+    }
+
+}
